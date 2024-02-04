ffb2b569e7f3281d26dba7b889e7fb80ab0a9475
benjyalper <benjyalper@hotmail.com>
2024-02-04 22:04:40 +0200

    Prepare fo Heroku deployment

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import moment from 'moment';
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Create a MySQL connection pool
 const pool = mysql.createPool({
